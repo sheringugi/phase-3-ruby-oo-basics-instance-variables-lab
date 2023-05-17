3263f9615b96fa70495752c2439f7f6261328ef9
sheringugi <sheri.ngugi17@gmail.com>
2023-05-17 11:32:52 +0300

    complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    # setter
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    
+    # getter
+    def name
+        @this_dogs_name
+    end
 
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
